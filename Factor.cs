4097b65d869ecfcde6c730254ec4d91e0b5d3826
Nitya <gorenityanand96@gmail.com>
2022-01-30 19:46:12 +0530

    Factor chnages done

--- Factor.cs
+++ Factor.cs
@@ -6,7 +6,9 @@ namespace FunctionalProgramming
     {
         public void factorPrime()
         {
+            
             Console.WriteLine("Enter the number:");
+
             int num = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 2; i*i <= num; i++)
